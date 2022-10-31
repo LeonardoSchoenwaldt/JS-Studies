6dbae6ed093347c5fa9c2dcb680938b304ab6a3c
Leonardo Schoenwaldt <leonardoschoewaldt@gmail.com>
2022-10-31 02:06:42 -0300

    VariaveisLet

---
+++ Aula04-VariaveisLet/Index.js
@@ -0,0 +1,17 @@
+let nome; // Declarar variável
+nome='Jair' // Iniciar variável
+//let nome='Jair';
+
+console.log(nome, 'nasceu em 1983.');
+console.log('Em 1999', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2007.');
+console.log('Maria teve um filho com', nome, 'em 2010.');
+console.log(nome, 'morreu.');
+
+// Não criar variável com palavras reservadas pela linguagem
+// Variáveis precisam ter nomes significativos (evitar confusões)
+// Não iniciar nome de variável com número
+// Não colocar traços ou espaços em nome de variável
+// Utilizar camelCase (palavras compostas iniciar com letra maiúscula ex: nomeCliente1)
+// Case-sensitive (letras maiúsculas e minúsculas causam diferença)
+// Não redeclarar variáveis com let
