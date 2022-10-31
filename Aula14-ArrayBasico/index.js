a4d0cb4591c301964a7909180ecc07e4d6869345
Leonardo Schoenwaldt <leonardoschoewaldt@gmail.com>
2022-10-31 19:38:01 -0300

    Array básico

---
+++ Aula14-ArrayBasico/index.js
@@ -0,0 +1,10 @@
+//              0        1       2      Índices separados pelos nomes
+const nomes = ['Luiza', 'João', 'Bianca']; 
+
+//nomes[0] = 'Jair' Altera um índice
+//nomes[3] = 'Moacir' Adiciona um índice
+//nomes.push('Olga'); Adiciona índice ao final do array
+//nomes.unshift('Eduarda'); Adiciona índice no começo do array
+//nomes.pop(); Remove o índice final do array
+//nomes.shift(); Remove o índice inicial do array
+//delete nomes[2] Deleta o índice deixando um espaço vazio
