8312d42bb692391656ae9d9fd80a44a3778cde4b
Leonardo Schoenwaldt <leonardoschoewaldt@gmail.com>
2022-10-31 02:01:52 -0300

    Comentários

---
+++ Aula02-Comentarios/comentarios.js
@@ -0,0 +1,9 @@
+// Duas barras = comentários(não afeta a linha de código)
+console.log('Hello World!');
+console.log('Hello World!');
+//console.log('Hello World!'); linha de código ignorada
+console.log('Hello World!');
+console.log('Hello World!');
+/* Barra + asterisco = comentário em bloco
+linha de comentário
+linha de comentário...*/
