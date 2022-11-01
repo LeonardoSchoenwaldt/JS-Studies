7ac9bd97f0a405872f88958f21bba08cb58c927a
Leonardo Schoenwaldt <leonardoschoewaldt@gmail.com>
2022-11-01 02:39:14 -0300

    Funções básico

---
+++ Aula15-FuncoesBasico/Index.js
@@ -0,0 +1,18 @@
+/*function cumprimento(nome) {
+    return `Bom dia, ${nome}!`;
+}
+const variavel = cumprimento('Leonardo');
+console.log(variavel);*/
+
+/*function soma(x = 1, y = 1) {
+    const resultado = x + y;
+    return resultado;
+}
+console.log(soma(19.43, 20.22));*/
+
+/*const raiz = function (n) {
+    return n ** 0.5;
+}
+console.log(raiz(9));*/
+
+//const raiz = (n) => n ** 0.5; ArrowFunction
