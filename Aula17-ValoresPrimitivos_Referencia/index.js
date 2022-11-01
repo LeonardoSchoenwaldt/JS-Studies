cd7aa10cb5c79b66b82554d1cc6bda81de88a076
Leonardo Schoenwaldt <leonardoschoewaldt@gmail.com>
2022-11-01 19:10:52 -0300

    Valores primitivos e por referência

---
+++ Aula17-ValoresPrimitivos_Referencia/index.js
@@ -0,0 +1,15 @@
+/* 
+Primitivos (imutáveis) - string, number, booblean, undefined, null, bigint, symbol
+Primitivos = valores copiados
+
+let a = 'A';
+let b = a;
+A variável b vai copiar o valor de a*/
+
+/*
+Referência (mutável) - array, object, funcition
+Referência = valores passados por referência 
+
+let a = [1, 2, 3];
+let b = a; 
+A variavel b vai receber o valor direto do array*/
