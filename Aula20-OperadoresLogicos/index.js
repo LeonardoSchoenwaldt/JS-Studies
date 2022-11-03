c92b0520b7ac9a5e40d6013709eac16a0c439a82
Leonardo Schoenwaldt <leonardoschoewaldt@gmail.com>
2022-11-03 00:29:55 -0300

    Operadores lógicos

---
+++ Aula20-OperadoresLogicos/index.js
@@ -0,0 +1,3 @@
+console.log(true && true && true); // &&(and) todas expressões precisam ser verdadeiras para retornar true
+console.log(false || false || false); // ||(or) todas expressões precisam ser falsas para retornar false
+console.log(!false); // !(not) inverte 
