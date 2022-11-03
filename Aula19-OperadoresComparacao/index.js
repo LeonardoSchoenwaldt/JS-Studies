00eedd6215bc3b460455048e4a0ac6fca9e63173
Leonardo Schoenwaldt <leonardoschoewaldt@gmail.com>
2022-11-02 23:23:09 -0300

    Operadores de comparação

---
+++ Aula19-OperadoresComparacao/index.js
@@ -0,0 +1,11 @@
+const num1 = 10;
+const num2 = 10;
+const comp1 = num1 > num2; // maior que
+const comp2 = num1 >= num2; // maior que ou igual a
+const comp3 = num1 < num2; // menor que
+const comp4 = num1 <= num2; // menor que ou igual a
+const comp5 = num1 == num2; // igualdade (valor)
+const comp6 = num1 === num2; // igualdade estrita (valor e tipo)
+const comp7 = num1 != num2; // diferença (valor)
+const comp8 = num1 !== num2; // diferença estrita (valor e tipo)
+console.log(comp8);
